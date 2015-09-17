3b5684534d5118bf3245a469da3be664751331c8
Carlos López <clopez3@gmail.com>
2015-09-16 19:14:03 -0500

    Primer archivo .rb con ejemplos de variables

---
+++ variables.rb
@@ -0,0 +1,21 @@
+# Así se hacen comentarios
+
+# A cada variable o línea le puedo agregar un comentario para documentar
+estatura = 1.85
+peso = 86
+edad = 40
+multiploTres = 6
+nombre = "Carlos"
+apellido = "López"
+
+=begin 
+	Este es un comentario en bloque, se define con =begin y =end,
+	todo lo que está adentro será tomado como un comentario
+=end
+
+puts estatura
+puts peso
+puts edad
+puts multiploTres
+puts nombre
+puts apellido
