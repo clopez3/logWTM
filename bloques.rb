0b41a172b1fb1400ff5f9b369ee7f0c518c2b1e6
Carlos López <clopez3@gmail.com>
2015-09-16 21:39:01 -0500

    elsif y validación

--- bloques.rb
+++ bloques.rb
@@ -4,18 +4,21 @@
 	Si no uso == sino = le asigno el mismo valor no lo comparo
 =end
 
+puts "Ejecutando línea número 7"
 if "Carlos" == "Carlos"
 	puts "Carlos es igual a Carlos"
 end
 
 #Provocando un error
 
+puts "Ejecutando línea número 14"
 if "Carlos" == "Andrés"
 	puts "Andrés es igual a Andrés"
 end
 
 #Rescatando el valor que comparamos
 
+puts "Ejecutando línea número 21"
 if "Carlos" == "Andrés"
 	puts "Carlos es igual a Andrés"
 else
@@ -24,7 +27,18 @@ end
 
 #Construyendo un bloque para validar el resultado de la suma 2+5
 
+puts "Ejecutando línea número 30"
 if 7 == 2+5
 	puts "El resultado es 7"
 end
 
+#Bloque con 2 condiciones
+
+puts "Ejecutando línea número 37"
+if 3 == 2
+	puts "3 es igual a 2"
+elsif 2 == 2
+	puts "2 es igual a 2"
+else 
+	puts "No se cumplió niguna condición"
+end
