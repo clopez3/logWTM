cb9b591fe078f14b8bdded458c6d02ddd80900cc
Carlos López <clopez3@gmail.com>
2015-09-18 14:27:01 -0500

    ejercicio 5

---
+++ ejercicio5.rb
@@ -0,0 +1,30 @@
+# Realiza la comparación de 2 variables. # Si las variables NO son iguales debería imprimir un mensaje de alerta
+
+nombre = "Carlos"
+apellido = "López"
+
+if nombre != apellido
+	puts "No son iguales"
+end
+
+# Realiza la comparación de cuatro valores, cada uno de ellos debería cumplirse
+
+if "1" == "1" && "2" == "2"
+	puts "Ambos son iguales"
+end
+
+# Realiza la comparación de dos números enteros, si son iguales muestra un mensaje de alerta
+
+if 2 == 2
+	puts "2 es igual a 2"
+end
+
+# Suma dos números enteros que como resultado den 5 y luego verifica si el cinco es mayor o igual a 7
+
+cinco = (3 + 2)
+
+if cinco >= 7
+	puts "¿5 es mayor que 7?"
+elsif cinco <= 7
+		puts "5 NO es mayor que 7"
+end
