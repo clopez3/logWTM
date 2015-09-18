e261083bd1c2da23ea0286b0375c1f66c89ac8b2
Carlos López <clopez3@gmail.com>
2015-09-17 19:45:44 -0500

    ejemplo de errores

---
+++ ejercicio3.rb
@@ -0,0 +1,17 @@
+Nombre = "Carlos"
+nombre = "andres"
+EDAD = 23
+ciudad = "Bogota"
+
+# Bogota es igual a bogota
+# Debería ejecutar el puts
+if ciudad == "Bogota" then
+	puts "La ciudad es BOGOTA"
+end
+
+# El arreglo debería tener 5 elementos
+arreglo = ["Manazana", "Limon", 3, 2.4, "Pera"]
+
+
+# Quiero imprimir el último elemento del arreglo
+puts arreglo [4]
