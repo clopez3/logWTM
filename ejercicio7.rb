c8e407f74c073d9948eb1f76c57b21a9d46c5b30
Carlos López <clopez3@gmail.com>
2015-09-19 21:35:06 -0500

    ejemplo class bebe con metodos y argumentos

---
+++ ejercicio7.rb
@@ -0,0 +1,17 @@
+class Bebe
+	def initialize
+		puts "Bebé creado"
+	end
+
+	def llorar(llanto)
+		puts "El bebé está llorando porque tiene #{llanto}."
+	end
+
+	def dormir(duerme)
+		puts "El bebé está durmiendo #{duerme}"
+	end
+
+	def comer(comida)
+		puts "El bebé se despertó y está comiendo #{comida}."
+	end
+end
